24d550d15e728acffed4bb39b9b7108fd2bd79bd
Sjoerd Bruijn <s.m.bruijn@gmail.com>
2024-12-07 19:06:52 +0100

    Update getandroiddata_hyperIMU.m

--- Phone functions/getandroiddata_hyperIMU.m
+++ Phone functions/getandroiddata_hyperIMU.m
@@ -16,28 +16,31 @@ t       = nan;
 acc     = nan(1,3);
 om      = nan(1,3);
 mag     = nan(1,3);
-data    = str2num(data);
-n=size(data,2);
-if mod(n,3)==1
-    t=data(1);
-    if n>1
-        acc=data(2:4);
+try
+    data    = str2num(data);
+    n=size(data,2);
+    if mod(n,3)==1
+        t=data(1);
+        if n>1
+            acc=data(2:4);
+        end
+        if n>4
+            om=data(5:7);
+        end
+        if n>7
+            mag=data(8:10);
+        end
+    else
+        if n>1
+            acc=data(1:3);
+        end
+        if n>3
+            om=data(4:6);
+        end
+        if n>6
+            mag=data(7:10);
+        end
     end
-    if n>4
-        om=data(5:7);
-    end
-    if n>7
-        mag=data(8:10);
-    end
-else
-    if n>1
-        acc=data(1:3);
-    end
-    if n>3
-        om=data(4:6);
-    end
-    if n>6
-        mag=data(7:10);
-    end
+catch
+    disp('could not fetch data')
 end
-
